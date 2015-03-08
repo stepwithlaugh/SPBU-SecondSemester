5e68471caf7f9aad48317fbba6072e0f9a18ad16
dankrupsky <dan.krupsky@gmail.com>
2015-03-08 10:05:38 +0300

    iterator boost fs

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
+// Dan Krupsky, 108
 #include <iostream>
+#include "pbhash.pb.h"
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+
+void get_dir_list(fs::directory_iterator iterator) {
+	for (; iterator != fs::directory_iterator(); ++iterator)
+	{
+		std::cout << (iterator->path().filename()) << fs::is_directory(iterator->status()) << std::endl;
+		if (fs::is_directory(iterator->status())) {
+			std::cout << "KIK" << std::endl;
+			fs::directory_iterator sub_dir(iterator->path());
+			get_dir_list(sub_dir);
+
+		}
+
+	}
+}
 
 int main() {
-	std::cout << "Hello World" << std::endl;
-	cit.get();
+	fs::directory_iterator home_dir(std::string("."));
+	get_dir_list(home_dir);
+	std::cin.get();
 	return 0;
 }
